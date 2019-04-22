a597711e07c5e017dd8ecf33084eda924a4a428a
Puja <csta.puja@gmail.com>
2019-04-22 17:32:54 +0545

    Splash Screen with animation added

---
+++ app/src/main/java/com/example/splashscreenpaperonboarding/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.splashscreenpaperonboarding;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import com.ramotion.paperonboarding.PaperOnboardingFragment;
+import com.ramotion.paperonboarding.PaperOnboardingPage;
+import com.ramotion.paperonboarding.listeners.PaperOnboardingOnRightOutListener;
+import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+
+        final PaperOnboardingFragment onBoardingFragment = PaperOnboardingFragment.newInstance(getDataForOnboarding());
+
+        androidx.fragment.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.add(R.id.fragment_container, onBoardingFragment);
+        fragmentTransaction.add(R.id.fragment_container, onBoardingFragment);
+        fragmentTransaction.commit();
+
+        onBoardingFragment.setOnRightOutListener(new PaperOnboardingOnRightOutListener() {
+            @Override
+            public void onRightOut() {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Fragment bf = new Fragment();
+                fragmentTransaction.replace(R.id.fragment_container, bf);
+                fragmentTransaction.commit();
+            }
+        });
+    }
+
+    private ArrayList<PaperOnboardingPage> getDataForOnboarding() {
+        // prepare data
+        PaperOnboardingPage scr1 = new PaperOnboardingPage("Hotels", "All hotels and hostels are sorted by hospitality rating",
+                Color.parseColor("#678FB4"), R.drawable.slider_image1, R.drawable.slider_image2);
+        PaperOnboardingPage scr2 = new PaperOnboardingPage("Banks", "We carefully verify all banks before add them into the app",
+                Color.parseColor("#65B0B4"), R.drawable.slider_image2, R.drawable.slider_iamge3);
+        PaperOnboardingPage scr3 = new PaperOnboardingPage("Stores", "All local stores are categorized for your convenience",
+                Color.parseColor("#9B90BC"), R.drawable.slider_iamge3, R.drawable.slider_image2);
+
+        ArrayList<PaperOnboardingPage> elements = new ArrayList<>();
+        elements.add(scr1);
+        elements.add(scr2);
+        elements.add(scr3);
+        return elements;
+    }
+}
